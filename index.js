28a5d5adb45961d7d81a92a2ef91ee3c858930e7
josueJURE <josue.jure@gmail.com>
2021-04-16 17:46:18 +0100

    create an array containing the days of the week

--- index.js
+++ index.js
@@ -3,26 +3,18 @@ var personPaying = ""
 const btn = document.getElementById("btn");
 const people = ["pierre", "paul", "jacques", "henry" ];
 const date = new Date().getDay();
-console.log(date)
-
-
+const dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var today = dayOfTheWeek[date];
 
 btn.addEventListener("click", function() {
   if (date >= 0 && date < 2) {
-    personPaying = "today pierre is footing the bill";
+    personPaying = `today is ${today} so Pierre is footing the bill`;
   } else if (date >= 2 && date < 4) {
-    personPaying = "today paul is paying";
+    personPaying = `today is ${today} paul is paying`;
   } else if (date >= 4 && date < 6) {
-    personPaying = "Hi jacques although we are in Amsterdam we're not going ducth"
+    personPaying = `Hi jacques, today is ${today} so although we are in Amsterdam we're not going ducth`
   } else {
-    personPaying = "Hi Henry your turn to pay today"
+    personPaying = `Hi Henry, today is ${today}, your turn to pay today`
   }
   name.textContent = personPaying
 }, false)
-
-// function whoIsBuying() {
-//   let whoPays = Math.floor(Math.random() * people.length)
-//   return people[whoPays];
-// }
-//
-// name.textContent = whoIsBuying()
