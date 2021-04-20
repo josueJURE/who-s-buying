6cc279353f8813667039116080812c963007d4cc
josueJURE <josue.jure@gmail.com>
2021-04-20 14:02:27 +0100

    replace if statement by ternary operator

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ const daysOfWeek = Array.from({length: 7}, (item, i) => {
 var today = daysOfWeek[date];
 
 btn.addEventListener("click", function() {
-  if (date >= 0 && date < 2) {
-    personPaying = `today is ${today} so Pierre is footing the bill`;
-  } else if (date >= 2 && date < 4) {
-    personPaying = `today is ${today} paul is paying`;
-  } else if (date >= 4 && date < 6) {
-    personPaying = `Hi jacques, today is ${today} so although we are in Amsterdam we're not going ducth`
-  } else {
-    personPaying = `Hi Henry, today is ${today}, your turn to pay today`
-  }
+  date >= 0 && date < 2 ? personPaying = `today is ${today} so Pierre is footing the bill`
+  : date >= 2 && date < 4 ? personPaying = `today is ${today} paul is paying`
+  : date >= 4 && date < 6 ?   personPaying = `Hi jacques, today is ${today} so although we are in Amsterdam we're not going ducth`
+  : personPaying = `Hi Henry, today is ${today}, your turn to pay today`
   name.textContent = personPaying
 }, false)
