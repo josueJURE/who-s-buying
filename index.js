741aff6a1c54f6cc5074bb5e947415f5d853842e
josueJURE <josue.jure@gmail.com>
2021-05-04 11:55:30 +0100

    use cssFloat javaScript syntax and for loop  to float divs to the left when user clicks on button

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var personPaying = ""
 const btn = document.getElementById("btn");
 const people = ["pierre", "paul", "jacques", "henry" ];
 const date = new Date().getDay();
+const floatContainer = document.getElementById("floatContainer");
+const container2 = document.getElementsByClassName("container2")[0];
+const floatLeft = document.getElementsByClassName("floatLeft")
+
+
 const daysOfWeek = Array.from({length: 7}, (item, i) => {
   return new Date(0,0,i).toLocaleDateString("en-US", {weekday: "long"})
 })
@@ -28,4 +33,11 @@ btn.addEventListener("click", function() {
   var unseen = document.getElementsByClassName("unseen");
   var unseenArray = [...unseen]
   unseenArray.map(x => x.style.display = "inline");
+  document.getElementsByClassName("container1")[0].style.height = "920px";
+  btn.style.display = "none";
+  container2.style.display = "block";
+  for(var i = 0; i < floatLeft.length; i++) {
+    floatLeft[i].style.cssFloat = "left"
+  }
+  document.getElementsByTagName("h1")[0].textContent = `how would you like to pay ${names[today]}`
 }, false);
