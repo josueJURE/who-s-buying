a5976d805e06dc4d75f55c7913b295a81f37f3a2
josueJURE <josue.jure@gmail.com>
2021-05-05 06:02:53 +0100

    use spread operator and replace for loop by map() method

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const name = document.getElementById("name");
 const image = document.getElementById("image");
-var personPaying = ""
 const btn = document.getElementById("btn");
 const people = ["pierre", "paul", "jacques", "henry" ];
 const date = new Date().getDay();
-const floatContainer = document.getElementById("floatContainer");
-const container2 = document.getElementsByClassName("container2")[0];
-const floatLeft = document.getElementsByClassName("floatLeft")
-
-
 const daysOfWeek = Array.from({length: 7}, (item, i) => {
   return new Date(0,0,i).toLocaleDateString("en-US", {weekday: "long"})
 })
 var today = daysOfWeek[date];
-var names = {
+const names = {
   "Monday": "emilie",
   "Tuesday": "emilie",
   "Wednesday": "jessica",
@@ -22,22 +15,36 @@ var names = {
   "Saturday": "paul",
   "Sunday": "pierre",
 }
+
 btn.addEventListener("click", function() {
   date >= 0 && date < 2 ? personPaying = `today is ${today} so ${names[today]} is footing the bill`
   : date >= 2 && date < 4 ? personPaying = `today is ${today} ${names[today]} is paying`
   : date >= 4 && date < 6 ?   personPaying = `Hi ${names[today]}, today is ${today} so although we are in Amsterdam we're not going ducth`
   : personPaying = `Hi ${names[today]}, today is ${today}, your turn to pay today`
-  name.textContent = personPaying;
+
+  btn.style.display = "none";
+
+  <!-- message to the person paying -->
+  let message = document.getElementsByClassName("message")[0];
+  message.style.display = "block";
+  message.textContent = personPaying;
+
+  <!--avatar of person paying -->
   image.src = `picture/${names[today]}.png`;
   image.alt = "picture of an avatar";
-  var unseen = document.getElementsByClassName("unseen");
-  var unseenArray = [...unseen]
+
+  <!--displaying <i/> tags -->
+  var unseenArray = [...document.getElementsByClassName("unseen")]
   unseenArray.map(x => x.style.display = "inline");
-  document.getElementsByClassName("container1")[0].style.height = "920px";
-  btn.style.display = "none";
-  container2.style.display = "block";
-  for(var i = 0; i < floatLeft.length; i++) {
-    floatLeft[i].style.cssFloat = "left"
-  }
-  document.getElementsByTagName("h1")[0].textContent = `how would you like to pay ${names[today]}`
+
+  <!--displaying containers and setting height -->
+  let containers = [...document.getElementsByClassName("containers")];
+  containers.map(x => x.style.display = "block");
+  containers.map(x => x.style.height = "920px");
+
+
+  <!-- floating divs with class container to left -->
+  containers.map(x => x.style.cssFloat = "left")
+
+  document.getElementsByTagName("h1")[0].textContent = `Hi ${names[today]} select you mehtod of payment`
 }, false);
